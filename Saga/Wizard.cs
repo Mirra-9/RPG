0eb4d72fda0504f229e9573b6783dc6aeac139ff
Mirra__ <92075214+Mirra-9@users.noreply.github.com>
2022-12-02 14:59:14 +0300

    Update Wizard.cs

--- Saga/Wizard.cs
+++ Saga/Wizard.cs
@@ -12,7 +12,6 @@ namespace CourseApp
 
         public override int Ability(Player player, Player enemy)
         {
-            player.StandTime = 2;
             enemy.StandTime = 2;
             Logger.LogText($"({Class}) {Name} использовал способность {AbilityName}");
             return 0;
